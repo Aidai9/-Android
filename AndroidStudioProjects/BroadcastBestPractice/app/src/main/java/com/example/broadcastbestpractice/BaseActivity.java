44d706af53d5dd0f9eb20bdd84ee2ab861a182f5
Aidai9 <965165514@qq.com>
2020-01-14 14:29:41 +0800

    “第一次提交”

---
+++ AndroidStudioProjects/BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/BaseActivity.java
@@ -0,0 +1,48 @@
+package com.example.broadcastbestpractice;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+}
+
+
+package com.example.broadcastbestpractice;
+
+        import android.app.Activity;
+
+        import java.util.ArrayList;
+        import java.util.List;
+
+public class ActivityCollector {
+    public static List<Activity> activities = new ArrayList<>();
+
+    public static void addActivity(Activity activity){
+        activities.add(activity);
+    }
+
+    public static void removeActivity(Activity activity){
+        activities.remove(activity);
+    }
+
+    public static void finishAll(){
+        for (Activity activity : activities){
+            if(!activity.isFinishing()){
+                activity.finish();
+            }
+        }
+    }
+}
